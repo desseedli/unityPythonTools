d60571c7035fff814a219b980b42ddd8374e34df
lixiangsheng <desseedli@gmail.com>
2023-08-15 14:04:43 +0800

    update

--- findKeyInExcel.py
+++ findKeyInExcel.py
@@ -3,6 +3,7 @@ import os
 
 file_path = './excel'
 find_list = ['rmb', 'RMB']
+res_path = []
 
 file_list = os.listdir(file_path)
 for file_name in file_list:
@@ -10,11 +11,16 @@ for file_name in file_list:
         file_name_no_extension, extension = os.path.splitext(file_name)
         open_file_path = os.path.join(file_path, file_name)
         workbook = openpyxl.load_workbook(open_file_path, data_only=True)
+        print('open ' + file_name_no_extension)
         table = workbook.worksheets[0]
         max_column = table.max_column
         read_row = 2
         for i in range(1, max_column + 1):
             value = table.cell(2, i).value
-            if value.lower() in find_list:
-                print(file_name_no_extension)
+            if value in find_list:
+                res_path.append(file_name_no_extension)
+                #print(file_name_no_extension)
                 break
+
+for res in res_path:
+    print(res)
